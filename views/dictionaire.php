25fbaf2927d40c4eea0b662a9eb62ecad334ff2a
MatthieuLemaire <lemairematthieudecergywanadoo.fr>
2018-04-12 11:49:25 +0200

    first commit

---
+++ views/dictionaire.php
@@ -0,0 +1,275 @@
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Dicossimo</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
+    <link rel="stylesheet" href="../assets/css/main.css">
+    <link rel="stylesheet" href="../assets/css/dictionaire.css">
+
+    <script defer src="https://use.fontawesome.com/releases/v5.0.10/js/all.js" integrity="sha384-slN8GvtUJGnv6ca26v8EzVaR9DC58QEwsIk9q1QXdCU8Yu8ck/tL/5szYlBbqmS+" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
+
+</head>
+<body>
+<main>
+    <div class="background-gradiant viewport-size position-fixed under"></div>
+    <header class="navbar shadow position-fixed d-flex justify-content-between align-items-center pr-3">
+        <a class="logo" href="../index.html">
+            <img src="../assets/images/logo.png" alt="logo retour au menu">
+        </a>
+        <button class="small-search" data-toggle="modal" data-target="#exampleModal">
+            <div>
+                <i class="fa fa-search text-white"></i>
+            </div>
+        </button>
+    </header>
+    <section class="container-fluid">
+        <ul>
+            <li>
+                <button type="button" data-toggle="collapse" data-target="#a" aria-expanded="false" aria-controls="a">
+                    a
+                </button>
+                <div class="collapse" id="a">
+                    aucun mot référencé pour le moment
+                </div>
+            </li>
+            <li>
+                <button type="button" data-toggle="collapse" data-target="#b" aria-expanded="false" aria-controls="b">
+                    b
+                </button>
+                <div class="collapse" id="b">
+                    aucun mot référencé pour le moment
+                </div>
+            <li>
+                <button type="button" data-toggle="collapse" data-target="#c" aria-expanded="false" aria-controls="c">
+                    c
+                </button>
+                <div class="collapse" id="c">
+                    aucun mot référencé pour le moment
+                </div>
+            </li>
+            <li>
+                <button type="button" data-toggle="collapse" data-target="#d" aria-expanded="false" aria-controls="d">
+                    d
+                </button>
+                <div class="collapse" id="d">
+                    aucun mot référencé pour le moment
+                </div>
+            </li>
+            <li>
+                <button type="button" data-toggle="collapse" data-target="#e" aria-expanded="false" aria-controls="e">
+                    e
+                </button>
+                <div class="collapse" id="e">
+                    aucun mot référencé pour le moment
+                </div>
+            </li>
+            <li>
+                <button type="button" data-toggle="collapse" data-target="#f" aria-expanded="false" aria-controls="f">
+                    f
+                </button>
+                <div class="collapse" id="f">
+                    aucun mot référencé pour le moment
+                </div>
+            </li>
+            <li>
+                <button type="button" data-toggle="collapse" data-target="#g" aria-expanded="false" aria-controls="g">
+                    g
+                </button>
+                <div class="collapse" id="g">
+                    aucun mot référencé pour le moment
+                </div>
+            </li>
+            <li>
+                <button type="button" data-toggle="collapse" data-target="#h" aria-expanded="false" aria-controls="h">
+                    h
+                </button>
+                <div class="collapse" id="h">
+                    aucun mot référencé pour le moment
+                </div>
+            </li>
+            <li>
+                <button type="button" data-toggle="collapse" data-target="#i" aria-expanded="false" aria-controls="i">
+                    i
+                </button>
+                <div class="collapse" id="i">
+                    aucun mot référencé pour le moment
+                </div>
+            </li>
+            <li>
+                <button type="button" data-toggle="collapse" data-target="#j" aria-expanded="false" aria-controls="j">
+                    j
+                </button>
+                <div class="collapse" id="j">
+                    aucun mot référencé pour le moment
+                </div>
+            </li>
+            <li>
+                <button type="button" data-toggle="collapse" data-target="#k" aria-expanded="false" aria-controls="k">
+                    k
+                </button>
+                <div class="collapse" id="k">
+                    aucun mot référencé pour le moment
+                </div>
+            </li>
+            <li>
+                <button type="button" data-toggle="collapse" data-target="#l" aria-expanded="false" aria-controls="l">
+                    l
+                </button>
+                <div class="collapse" id="l">
+                    aucun mot référencé pour le moment
+                </div>
+            </li>
+            <li>
+                <button type="button" data-toggle="collapse" data-target="#m" aria-expanded="false" aria-controls="m">
+                    m
+                </button>
+                <div class="collapse" id="m">
+                    aucun mot référencé pour le moment
+                </div>
+            </li>
+            <li>
+                <button type="button" data-toggle="collapse" data-target="#n" aria-expanded="false" aria-controls="n">
+                    n
+                </button>
+                <div class="collapse" id="n">
+                    aucun mot référencé pour le moment
+                </div>
+            </li>
+            <li>
+                <button type="button" data-toggle="collapse" data-target="#o" aria-expanded="false" aria-controls="o">
+                    o
+                </button>
+                <div class="collapse" id="o">
+                    aucun mot référencé pour le moment
+                </div>
+            </li>
+            <li>
+                <button type="button" data-toggle="collapse" data-target="#p" aria-expanded="false" aria-controls="p">
+                    p
+                </button>
+                <div class="collapse" id="p">
+                    aucun mot référencé pour le moment
+                </div>
+            </li>
+            <li>
+                <button type="button" data-toggle="collapse" data-target="#q" aria-expanded="false" aria-controls="q">
+                    q
+                </button>
+                <div class="collapse" id="q">
+                    aucun mot référencé pour le moment
+                </div>
+            </li>
+            <li>
+                <button type="button" data-toggle="collapse" data-target="#r" aria-expanded="false" aria-controls="r">
+                    r
+                </button>
+                <div class="collapse" id="r">
+                    aucun mot référencé pour le moment
+                </div>
+            </li>
+            <li>
+                <button type="button" data-toggle="collapse" data-target="#s" aria-expanded="false" aria-controls="s">
+                    s
+                </button>
+                <div class="collapse" id="s">
+                    aucun mot référencé pour le moment
+                </div>
+            </li>
+            <li>
+                <button type="button" data-toggle="collapse" data-target="#t" aria-expanded="false" aria-controls="t">
+                    t
+                </button>
+                <div class="collapse" id="t">
+                    aucun mot référencé pour le moment
+                </div>
+            </li>
+            <li>
+                <button type="button" data-toggle="collapse" data-target="#u" aria-expanded="false" aria-controls="u">
+                    u
+                </button>
+                <div class="collapse" id="u">
+                    aucun mot référencé pour le moment
+                </div>
+            </li>
+            <li>
+                <button type="button" data-toggle="collapse" data-target="#v" aria-expanded="false" aria-controls="v">
+                    v
+                </button>
+                <div class="collapse" id="v">
+                    aucun mot référencé pour le moment
+                </div>
+            </li>
+            <li>
+                <button type="button" data-toggle="collapse" data-target="#w" aria-expanded="false" aria-controls="w">
+                    w
+                </button>
+                <div class="collapse" id="w">
+                    aucun mot référencé pour le moment
+                </div>
+            </li>
+            <li>
+                <button type="button" data-toggle="collapse" data-target="#x" aria-expanded="false" aria-controls="x">
+                    x
+                </button>
+                <div class="collapse" id="x">
+                    aucun mot référencé pour le moment
+                </div>
+            </li>
+            <li>
+                <button type="button" data-toggle="collapse" data-target="#y" aria-expanded="false" aria-controls="y">
+                    y
+                </button>
+                <div class="collapse" id="y">
+                    aucun mot référencé pour le moment
+                </div>
+            </li>
+            <li>
+                <button type="button" data-toggle="collapse" data-target="#z" aria-expanded="false" aria-controls="z">
+                    z
+                </button>
+                <div class="collapse" id="z">
+                    aucun mot référencé pour le moment
+                </div>
+            </li>
+        </ul>
+    </section>
+
+
+
+    <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModal" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-centered" role="document">
+            <div class="modal-content">
+                <div class="background-gradiant  d-flex flex-column align-items-center p-5">
+                    <div class="p-3">
+                        <h5 class="modal-title" id="exampleModalLabel">Cherchez un mot</h5>
+                    </div>
+                    <div>
+                        <input type="text" placeholder="recherche">
+                        <div class="d-flex flex-row mt-3"><h6>tags</h6><button class="add-tag">+</button></div>
+                    </div>
+                    <div>
+                        <button class="medium-search">
+                            <div>
+                                <i class="fa fa-search text-white"></i>
+                            </div>
+                        </button>
+                        <h4>C'est parti</h4>
+                    </div>
+                    <div>
+                        <button type="button" class="close btn" data-dismiss="modal">fermer</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</main>
+
+</body>
+</html>
